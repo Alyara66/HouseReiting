1f78951abe7d94402b316919920a6f1539bf54be
Viktor1806 <viktorgrigorov06@gmail.com>
2025-01-21 12:09:25 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseReiting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
